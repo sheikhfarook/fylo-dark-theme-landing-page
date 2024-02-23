39160e6426ada04af7ed8c7cff0e14350c89d4df
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-23 16:58:16 +0530

    complete the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,28 @@
 import logo from "../../assets/logo.svg";
+import StartPoint from "./startPoint";
 
 const Header = () => {
   return (
-    <div className="flex justify-between ">
-      <div>
-        <img src={logo} alt="" />
-      </div>
-      <div className="flex gap-10 text-[#FFF] items-center text-[16px] ">
-        <div className="hover:underline hover:font-[700] font-[100]">
-          {" "}
-          Features
+    <div className="m-14 max-sm:m-5">
+      <div className="flex justify-between ">
+        <div>
+          <img className="w-[80px]" src={logo} alt="" />
         </div>
-        <div className="font-[100] hover:underline hover:font-[700]">Team</div>
-        <div className="font-[100] hover:underline hover:font-[700]">
-          Sign In
+        <div
+          className="flex gap-10 text-[#FFF] items-center text-[16px] font-raleway
+        max-sm:text-[12px] max-sm:gap-6 ">
+          <div className="hover:underline hover:font-[700] font-[100]">
+            Features
+          </div>
+          <div className="font-[100] hover:underline hover:font-[700]">
+            Team
+          </div>
+          <div className="font-[100] hover:underline hover:font-[700]">
+            Sign In
+          </div>
         </div>
       </div>
+      <StartPoint />
     </div>
   );
 };

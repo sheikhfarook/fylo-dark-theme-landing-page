856c7aec2c7ac67c1f36efb92a9f4dd52f92923c
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-23 16:48:33 +0530

    import the font

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,14 +2,19 @@ import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex justify-between ">
       <div>
         <img src={logo} alt="" />
       </div>
-      <div>
-        <div>Features</div>
-        <div>Team</div>
-        <div>Sign In</div>
+      <div className="flex gap-10 text-[#FFF] items-center text-[16px] ">
+        <div className="hover:underline hover:font-[700] font-[100]">
+          {" "}
+          Features
+        </div>
+        <div className="font-[100] hover:underline hover:font-[700]">Team</div>
+        <div className="font-[100] hover:underline hover:font-[700]">
+          Sign In
+        </div>
       </div>
     </div>
   );

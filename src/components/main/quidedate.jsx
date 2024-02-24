0e349b73fbfc7b571703d97a1aac3bc27a15a28a
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-24 13:02:15 +0530

    create data

---
+++ src/components/main/quidedate.jsx
@@ -0,0 +1,32 @@
+import access from "../../assets/icon-access-anywhere.svg";
+import secure from "../../assets/icon-security.svg";
+import time from "../../assets/icon-collaboration.svg";
+import store from "../../assets/icon-any-file.svg";
+
+export const guide = [
+  {
+    id: 1,
+    img: access,
+    headline: "Access your files, anywhere",
+    info: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+  },
+  {
+    id: 2,
+    img: secure,
+    headline: "Security you can trust",
+    info: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.",
+  },
+  {
+    id: 3,
+    img: time,
+    headline: "Real-time collaboration",
+    info: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
+  },
+
+  {
+    id: 4,
+    img: store,
+    headline: "Store any type of file",
+    info: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+  },
+];

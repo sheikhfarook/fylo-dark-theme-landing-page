de4f2f83f53dcc5e54a8b7232f9aa4901bd66a19
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-26 12:09:33 +0530

    create new file

---
+++ src/components/main/getstarted.jsx
@@ -0,0 +1,23 @@
+const GetStarted = () => {
+  return (
+    <div>
+      <div className="w-[863px] h-[275px] rounded-[9px] bg-[#1b2330] shadow-2xl m-auto text-center text-[#FFF] ">
+        <div className="w-[346px] h-[48px] m-auto font-raleway text-[32px] font-[700] leading-[48px] ">
+          Get early access today
+        </div>
+        <div className="w-[678px] h-[42px] text-center m-auto font-opensans font-[300] text-[14px] leading-[21px] ">
+          It only takes a minute to sign up and our free starter tier is
+          extremely generous. If you have any questions, our support team would
+          be happy to help you.
+        </div>
+
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
